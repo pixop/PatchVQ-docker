da158a4a7c0cca10540813f2b025251f730de584
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2022-06-14 18:00:09 +0200

    Full functionality

--- script/run_all.sh
+++ script/run_all.sh
@@ -1,9 +1,47 @@
 #!/bin/bash
+rm -fR data/inference
 
-#rm -fR data/Bitmovin/features/*
+mkdir data/inference
 
-python extract_features_PaQ2PiQ.py
-python extract_features_resnet3d.py
-python pool_features.py
-python test.py
+# lossless transcoding of the input file
+echo
+echo "======================="
+echo "==== PREPROCESSING ===="
+echo "======================="
+echo
 
+echo "$2"
+
+ffmpeg $3 -i $1 $2 -c:v ffv1 -an -y data/inference/test.mov
+
+# probe number of frames
+NO_FRAMES=$(ffprobe -v error -select_streams v:0 -count_frames -show_entries stream=nb_read_frames -print_format default=nokey=1:noprint_wrappers=1 data/inference/test.mov)
+LAST_IMAGE=$(printf "test/image_%05d" $NO_FRAMES)
+
+echo "name,mos,is_valid,frame_number,fn_last_frame" > data/inference/labels.csv
+echo "test,0,False,$NO_FRAMES,$LAST_IMAGE" >> data/inference/labels.csv
+
+script/extract_mp4.sh data/inference/test.mov
+
+# extract and pool features
+echo
+echo "============================"
+echo "==== FEATURE EXTRACTION ===="
+echo "============================"
+echo
+
+python src/extract_features_PaQ2PiQ.py
+python src/extract_features_resnet3d.py
+python src/pool_features.py
+
+# insert duplicate record to avoid errors relating to SRCC/LCC requiring at least two records
+echo "test,0,False,$NO_FRAMES,$LAST_IMAGE" >> data/inference/labels.csv
+
+# inference
+echo
+echo "======================="
+echo "==== MOS INFERENCE ===="
+echo "======================="
+echo
+
+python src/inference.py
