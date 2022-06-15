fca156abd7fc830ff7d1a1c8a8f350cd3afd9e5c
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2022-06-15 13:05:22 +0200

    Drop intermediate transcoding step

--- script/run_all.sh
+++ script/run_all.sh
@@ -4,7 +4,6 @@
 rm -fR data/inference
 mkdir -p data/inference
 
-# transcode the input file losslessly to a new QuickTime container
 echo
 echo "======================="
 echo "==== PREPROCESSING ===="
@@ -14,20 +13,22 @@ echo
 echo "Input file: $1"
 echo "Output args: $2"
 echo "Input args: $3"
-echo
 
-ffmpeg -hide_banner -loglevel error $3 -i "$1" $2 -crf 0 -an -y data/inference/test.mov
+OUT_DIR="data/inference/png"
+NAME="test"
+
+mkdir -p $OUT_DIR/$NAME
+
+# extract PNGs
+ffmpeg -hide_banner -loglevel error $3 -i "$1" $2 $OUT_DIR/$NAME/image_%05d.png
 
 # probe number of frames
-NO_FRAMES=$(ffprobe -v error -select_streams v:0 -count_frames -show_entries stream=nb_read_frames -print_format default=nokey=1:noprint_wrappers=1 data/inference/test.mov)
-LAST_IMAGE=$(printf "test/image_%05d" $NO_FRAMES)
+NO_FRAMES=$(ffprobe -v error -select_streams v:0 -count_frames -show_entries stream=nb_read_frames -print_format default=nokey=1:noprint_wrappers=1 $OUT_DIR/$NAME/image_%05d.png)
+LAST_IMAGE=$(printf "$NAME/image_%05d" $NO_FRAMES)
 
 # create fake labels.csv file
 echo "name,mos,is_valid,frame_number,fn_last_frame" > data/inference/labels.csv
-echo "test,0,False,$NO_FRAMES,$LAST_IMAGE" >> data/inference/labels.csv
-
-# extract PNGs
-script/extract_mov.sh data/inference/test.mov
+echo "$NAME,0,False,$NO_FRAMES,$LAST_IMAGE" >> data/inference/labels.csv
 
 # extract and pool features
 echo
@@ -41,7 +42,7 @@ python3 src/extract_features_resnet3d.py
 python3 src/pool_features.py
 
 # insert duplicate record to avoid errors relating to SRCC/LCC requiring at least two records
-echo "test,0,False,$NO_FRAMES,$LAST_IMAGE" >> data/inference/labels.csv
+echo "$NAME,0,False,$NO_FRAMES,$LAST_IMAGE" >> data/inference/labels.csv
 
 # inference
 echo
